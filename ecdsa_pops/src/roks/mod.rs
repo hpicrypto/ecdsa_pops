6430284b85f6c70dcd03913e047300531439cd13
Alexandros Zacharakis <alexandroszacharakis8@gmail.com>
2026-04-21 08:58:43 +0200

    Add PA RoK

--- ecdsa_pops/src/roks/mod.rs
+++ ecdsa_pops/src/roks/mod.rs
@@ -4,5 +4,6 @@ pub(crate) mod bls_to_tom;
 pub(crate) mod circuit_rok;
 pub(crate) mod cschnorr_rok;
 pub(crate) mod dleq_rok;
+pub(crate) mod pa_rok;
 pub(crate) mod pedersen_rok;
 pub(crate) mod sm_rok;
